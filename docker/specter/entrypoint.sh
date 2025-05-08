1da05f11b376b21b0c0c5ab5cfcec445a15ffe61
Itamar Perez <itamarperez@gmail.com>
2025-05-08 10:47:55 -0700

    Update entrypoint.sh to generate separate configuration files for Spectrum and Knots nodes
    - Created a new configuration file for the Spectrum node at spectrum_node.json, enhancing support for different node types.
    - Updated the existing configuration for the Knots node to use knots_node.json, ensuring clarity and separation of node configurations.
    - Added echo statements to confirm the generation of both configuration files.

--- docker/specter/entrypoint.sh
+++ docker/specter/entrypoint.sh
@@ -3,17 +3,33 @@ set -e
 
 # Define the path for the nodes configuration
 NODE_CONFIG_DIR="/data/.specter/nodes"
-NODE_CONFIG_FILE="$NODE_CONFIG_DIR/knots.json"
+SPECTRUM_NODE_CONFIG_FILE="$NODE_CONFIG_DIR/spectrum_node.json"
+KNOTS_NODE_CONFIG_FILE="$NODE_CONFIG_DIR/knots_node.json"
 
 # Create the nodes directory if it doesn't exist
 mkdir -p "$NODE_CONFIG_DIR"
 
-# Create the node configuration file using runtime environment variables
+# Create the Spectrum node configuration file using runtime environment variables
 # Use default values if environment variables are not set (should not happen with docker-compose)
-cat <<EOF > "$NODE_CONFIG_FILE"
+cat <<EOF >"$SPECTRUM_NODE_CONFIG_FILE"
+{
+    "python_class": "cryptoadvance.specterext.spectrum.spectrum_node.SpectrumNode",
+    "fullpath": "/data/.specter/nodes/spectrum_node.json",
+    "name": "Spectrum Node",
+    "alias": "spectrum_node",
+    "host": "electrs",
+    "port": 50001,
+    "ssl": false
+}
+EOF
+
+echo "Generated Specter Spectrum node config at $SPECTRUM_NODE_CONFIG_FILE"
+
+# Create the Knots node configuration file
+cat <<EOF > "$KNOTS_NODE_CONFIG_FILE"
 {
   "python_class": "cryptoadvance.specter.node.Node",
-  "fullpath": "$NODE_CONFIG_FILE",
+  "fullpath": "$KNOTS_NODE_CONFIG_FILE",
   "name": "knots",
   "alias": "knots",
   "autodetect": false,
@@ -27,7 +43,7 @@ cat <<EOF > "$NODE_CONFIG_FILE"
 }
 EOF
 
-echo "Generated Specter node config at $NODE_CONFIG_FILE"
+echo "Generated Specter Knots node config at $KNOTS_NODE_CONFIG_FILE"
 
 # Execute the original command (passed as arguments to this script)
 exec /usr/local/bin/python3 -m cryptoadvance.specter server --host 0.0.0.0 --port 25441
